6f8441ea220482be4b650af1329efc6200c039c0
Rohan Kannan <2rohankannan@gmail.com>
2025-05-28 00:21:41 +1000

    fixed participant boxes

--- code/beastly-brawl-showdown/imports/ui/host/projector/ParticipantDisplayBox.tsx
+++ code/beastly-brawl-showdown/imports/ui/host/projector/ParticipantDisplayBox.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import { ParticipantBox } from './ParticipantBox';
 
 export const ParticipantDisplayBox = ({ name }: { name: string }) => {
+    const names = name.split(',').map(n => n.trim());
+
     return (
         <div className='participants-display-box'>
-            <div className="participants-header">
-                <div className="partcipants-count"></div>
-                <button className="glb-btn start-game-btn">Start Game</button>
-            </div>
-
-            <div className="participants-grid">
-                <ParticipantBox name={name}/>
-            </div>
-            
+            {names.map((n) => (
+                <ParticipantBox key={n} name={n} />
+            ))}
         </div>
     )
 }
