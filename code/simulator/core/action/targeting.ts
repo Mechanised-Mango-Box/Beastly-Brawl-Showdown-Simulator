ec8433ec496b35b138b812f1024e2ca68baa3667
Mechanised Mango Box <mechanised_mango_box@proton.me>
2025-08-08 09:34:01 +1000

    Remove unused targeting methods

--- code/simulator/core/action/targeting.ts
+++ code/simulator/core/action/targeting.ts
@@ -1,6 +1,5 @@
 import { SideId } from "../side";
 
-
 /**
  * The following targeting methods are availible
  */
@@ -8,10 +7,12 @@ export type TargetingMethod = "self" | "single-enemy" | "all-enemy" | "all";
 interface BaseTargetingData<TM extends TargetingMethod> {
   targetingMethod: TM;
 }
-export interface SelfTargeting extends BaseTargetingData<"self"> { }
-export interface SingleEnemyTargeting extends BaseTargetingData<"single-enemy"> {
+export interface SelfTargeting extends BaseTargetingData<"self"> {}
+export interface SingleEnemyTargeting
+  extends BaseTargetingData<"single-enemy"> {
   target: SideId;
 }
-interface AllEnemyTargeting extends BaseTargetingData<"all-enemy"> { }
-interface AllTargeting extends BaseTargetingData<"all"> { }
-export type TargetingData = SelfTargeting | SingleEnemyTargeting | AllEnemyTargeting | AllTargeting;
+// // interface AllEnemyTargeting extends BaseTargetingData<"all-enemy"> { }
+// // interface AllTargeting extends BaseTargetingData<"all"> { }
+export type TargetingData = SelfTargeting | SingleEnemyTargeting;
+// //| AllEnemyTargeting | AllTargeting;
