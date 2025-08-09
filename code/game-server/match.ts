6de7cf3e4b8797b97d5e31499fac144f7a96c3f3
Jamie Kaltzis <jamiekaltzis02@gmail.com>
2025-08-09 13:48:57 +1000

    Rename types to match file refactoring

--- code/game-server/match.ts
+++ code/game-server/match.ts
@@ -1,24 +1,24 @@
 import { Player } from "./player";
 import { AccountId } from "./types";
 
-enum BattleType {
+enum MatchType {
     DUEL,
     BYE
 }
 
-export class Battle {
+export class Match {
     player1: Player;
     player2?: Player;
     winnerId?: AccountId;
     spectators: AccountId[];
-    battleType: BattleType;
+    battleType: MatchType;
     battleID: number;
 
     constructor(player1: Player, battleID: number, player2?: Player) {
         this.player1 = player1;
         this.player2 = player2;
         this.spectators = player1.spectators.concat(player2?.spectators ?? []);
-        this.battleType = player2 ? BattleType.DUEL : BattleType.BYE;
+        this.battleType = player2 ? MatchType.DUEL : MatchType.BYE;
         this.battleID = battleID;
     }
 }
