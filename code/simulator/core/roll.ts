6cde515e9530542e2eb1727b2e3a36412f1a2167
Harshil J Patel <hpat0037@student.monash.edu>
2025-08-13 20:03:59 +1200

    Updated roll to use PRNG

--- code/simulator/core/roll.ts
+++ code/simulator/core/roll.ts
@@ -1,3 +1,10 @@
+import { PRNG } from "./prng";
+
+let rng: PRNG;
+export function initRolls(prng: PRNG) {
+  rng = prng;
+}
+
 export function roll(sides: number): number {
-  return Math.floor(Math.random() * sides) + 1;
+  return Math.floor(rng.next() * sides) + 1;
 }
