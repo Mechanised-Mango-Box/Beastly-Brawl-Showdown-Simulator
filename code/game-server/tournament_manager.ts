5f5ebbfe8b0296eb4e7f48e34c6279328841f09d
Jamie Kaltzis <jamiekaltzis02@gmail.com>
2025-08-15 09:42:55 +1000

    Remove result handling and add call to matches. - Removed reportmatchResults() and modified checkRoundCompletion() as battle result handling is a responsibility of Match. - Added a Promise.all statement in startTournament() to signal to all Matches to start combat at the same time.

--- code/game-server/tournament_manager.ts
+++ code/game-server/tournament_manager.ts
@@ -25,34 +25,6 @@ export class TournamentManager {
     console.log(`Created ${this.matches.length} matchs for this round.`);
   }
 
-  // To be called immediately after a match ends
-  async reportmatchResults(winnerId: string, matchID: number): Promise<void> {
-    // Find the match by ID
-    const match = this.matches.find(b => b.matchID === matchID);
-    if (!match) {
-      throw new Error(`match with ID ${matchID} not found.`);
-    }
-
-    match.winnerId =  winnerId;
-
-    // Retrieve the loser's AccountId
-    const loserId = match.player1.linkedAccountId === winnerId
-      ? match.player2?.linkedAccountId
-      : match.player1.linkedAccountId;
-
-    if (loserId) {
-      const loserPlayer = this.playersByAccountId.get(loserId);
-      if (loserPlayer) {
-        loserPlayer.addSpectator(winnerId);
-      }
-    }
-
-    // Remove completed match
-    this.matches = this.matches.filter(b => b.matchID !== matchID);
-
-    await this.checkRoundCompletion();
-  }
-
   async checkRoundCompletion(): Promise<void> {
     // Check if all matchs in the current round have winners
     const allCompleted = this.matches.every(match => match.winnerId);
@@ -63,12 +35,8 @@ export class TournamentManager {
     // If all matchs are completed, determine winners and create new matchs for the next round
     const winners: Player[] = [];
     for (const match of this.matches) {
-      const winnerId = match.winnerId;
-      if (winnerId) {
-        const winner = this.playersByAccountId.get(winnerId);
-        if (winner) {
-          winners.push(winner);
-        }
+      if (match.winner) {
+        winners.push(match.winner)
       }
     }
 
@@ -87,6 +55,8 @@ export class TournamentManager {
   async startTournament(): Promise<void> {
     const players = Array.from(this.playersByAccountId.values());
     this.creatematchs(players);
-  }
 
+    await Promise.all(this.matches.map(match => match.startBattle(this.playersByAccountId)))
+    await this.checkRoundCompletion()
+  }
 }
