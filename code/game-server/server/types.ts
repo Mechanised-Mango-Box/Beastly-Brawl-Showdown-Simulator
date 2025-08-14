764249b7ba2a5f1cf13fd729ec05988745b3a964
Rohan Kannan <2rohankannan@gmail.com>
2025-08-15 09:18:43 +1000

    deleted gameserverv1, seems to work?

--- code/game-server/server/types.ts
+++
@@ -1,7 +0,0 @@
-// TODO export to all folders / make global across services
-
-export type ServerId = number;
-export type RoomId = number;
-export type JoinCode = string;
-
-export type AccountId = string;
