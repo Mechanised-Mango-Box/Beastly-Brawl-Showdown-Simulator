039d99ae12925b2fbbdf60fc926dbfc6965e81ad
Joe <tsunetoishida03@gmail.com>
2025-08-15 10:07:52 +1000

    Initial setup for loading page complete

---
+++ code/beastly-brawl-showdown/imports/ui/BattleScreen/battleLoadingScreen/PlayerAvatar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const PlayerAvater = () => {
+    return (
+        <div className="player-avatar">
+            {/* img of player avatar here */}
+        </div>
+    )
+}
