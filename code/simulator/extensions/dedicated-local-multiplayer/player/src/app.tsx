f0a0d1d029a54c5c9d78458b08b4149a1423946c
henryalditio <henryalditio@gmail.com>
2025-08-16 18:22:44 +1000

    Removed unused line from app.tsx

--- code/simulator/extensions/dedicated-local-multiplayer/player/src/app.tsx
+++ code/simulator/extensions/dedicated-local-multiplayer/player/src/app.tsx
@@ -5,17 +5,6 @@ import GamePage from "./pages/game_page"; // an example other page
 import { SocketProvider } from "./socket/socket_provider";
 import BattleVisualiser from "../../../visualiser/BattleVisualiser"
 
-// const App: React.FC = () => (
-//   <SocketProvider>
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/join" element={<PlayerConnectionPage />} />
-//         <Route path="/" element={<GamePage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   </SocketProvider>
-// );
-
 // export default App;
 
 const App: React.FC = () => (
