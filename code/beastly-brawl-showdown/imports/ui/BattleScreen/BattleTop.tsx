5a251d545a8fc22bb5be4a9533c38c15f9d8dcf0
Madison Hem <mhem0008@student.monash.edu>
2025-08-22 09:34:04 +1000

    surrender button pre commit

--- code/beastly-brawl-showdown/imports/ui/BattleScreen/BattleTop.tsx
+++ code/beastly-brawl-showdown/imports/ui/BattleScreen/BattleTop.tsx
@@ -6,12 +6,10 @@ export const BattleTop: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="battleScreenTop">
-      <img className="topBackIcon" src="/img/back_line.png" alt="back" />
-      {/*should change the main to be the screen to go back too after surrendering */}
-      <button onClick={() => navigate('/main')} className="battleScreenTopButton">
-        Surrender
-      </button>
-    </div>
-  );
+  <button onClick={() => navigate('/main')} className="battleScreenTop-btn">
+    {/* <img className="topBackIcon" src="/img/back_line.png" alt="back" /> */}
+    Surrender
+  </button>
+);
+
 };
