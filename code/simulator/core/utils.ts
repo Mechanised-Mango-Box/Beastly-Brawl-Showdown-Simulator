2e0f3b6e4b1b43230786831158a7ebb5e7f853b2
Mechanised Mango Box <mechanised_mango_box@proton.me>
2025-08-23 23:02:06 +1000

    Updated Lookup Table with better comments

--- code/simulator/core/utils.ts
+++ code/simulator/core/utils.ts
@@ -1,5 +1,18 @@
 export type EntryID = Lowercase<string>;
 
-export type NamedRecord<ACCEPTED_KEYS extends PropertyKey, DATA, DISCRIMINANT_NAME extends PropertyKey> = {
-  [ROW_KEY in ACCEPTED_KEYS]: DATA & { [K in DISCRIMINANT_NAME]: ROW_KEY };
+/**
+ * A record-like type that enforces:
+ * - For a given set of keys (`ACCEPTED_KEYS`):
+ *   - All keys must be present (exhaustive)
+ * - Each key (`ROW_KEY`) maps to a `VALUE`:
+ *   - `VALUE` must contain a property named `DISCRIMINANT_NAME`
+ *   - The value of that property must match its corresponding `ROW_KEY`
+ *
+ * @template ACCEPTED_KEYS - Union of all keys to be present in the record
+ * @template DISCRIMINANT_NAME - Name of the discriminant property in each value
+ * @template VALUE - The object type assigned to each key, which must include `DISCRIMINANT_NAME`
+ */
+
+export type LookupTable<ACCEPTED_KEYS extends PropertyKey, DISCRIMINANT_NAME extends PropertyKey, VALUE extends { [K in DISCRIMINANT_NAME]: PropertyKey }> = {
+  [ROW_KEY in ACCEPTED_KEYS]: VALUE & { [K in DISCRIMINANT_NAME]: ROW_KEY };
 };
